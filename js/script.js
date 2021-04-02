dbe61e153fe42586518e0b840823bdd6da32601f
Yash Lohar <77219427+YashLohar@users.noreply.github.com>
2021-04-02 11:07:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
     $(".toggle").click(function(){
           $(".links-section").slideToggle(150);
     });
+
+    $(".angle-1").click(function(){
+        $(".submenu-1").slideToggle(150);
+  });
+  
+  $(".angle-2").click(function(){
+        $(".submenu-2").slideToggle(150);
+  });
+
+  $(".angle-3").click(function(){
+        $(".submenu-3").slideToggle(150);
+  });
 });
